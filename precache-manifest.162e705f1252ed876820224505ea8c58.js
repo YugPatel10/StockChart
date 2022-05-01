bc0e79702b38b0b6ef88bc7ebeef3fbc7590c6bf
Yug Patel <yugpatel1699@gmail.com>
2022-05-01 14:50:16 -0700

    Updates

---
+++ precache-manifest.162e705f1252ed876820224505ea8c58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7832fbc21f925502f46ab9646af5c83d",
+    "url": "/stockchart/index.html"
+  },
+  {
+    "revision": "b27de02f696ef9f8cbb8",
+    "url": "/stockchart/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3c1cd89c3c4d500445b8",
+    "url": "/stockchart/static/js/2.80bb184d.chunk.js"
+  },
+  {
+    "revision": "b27de02f696ef9f8cbb8",
+    "url": "/stockchart/static/js/main.6797ac42.chunk.js"
+  },
+  {
+    "revision": "2110d6bed3400492a2b7",
+    "url": "/stockchart/static/js/runtime~main.d979cfe1.js"
+  }
+]);
